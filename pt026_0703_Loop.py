91d56800bb57ff057fc9397d7a82a3b272ea7162
Wilasinee026 <68219010026@htc.ac.th>
2025-09-02 09:43:08 +0700

    Loop026

---
+++ pt026_0703_Loop.py
@@ -0,0 +1,12 @@
+i = 50
+
+while i >= 0:
+    print(i, end = ', ')
+    i = i - 5
+
+print()
+n = 1
+
+while n <= 10:
+    print(2 *n + 1, end = ', ')
+    n = n + 1
---
+++ pt026_0711_Loop.py
@@ -0,0 +1,11 @@
+site = 'marcuscode'
+for n in site:
+    print(n)
+
+name = ['Mateo','John','Eric','Mark','Robert']
+for n in name:
+    print(n)
+
+numbers = [10,20,30,40,50,60,70,80]
+for n in numbers:
+    print(n)
